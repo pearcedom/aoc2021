c9b8c1f8dc31f95fdd5bb35e21e4aca6e3b23265
pearcedom <dominic.pearce@fiosgenomics.com>
2021-12-01 14:13:25 +0000

    deduplicate

--- src/day01/solution.py
+++ src/day01/solution.py
@@ -1,12 +1,9 @@
-def part1(x):
-    return sum(j > i for i, j in zip(x[:-1], x[1:]))
-
-def part2(x):
-    return sum(j > i for i, j in zip(x[:-1], x[3:]))
+def adjacent_increases(x, offset = 1):
+    return sum(j > i for i, j in zip(x[:-1], x[offset:]))
 
 if __name__ == "__main__":
     with open("src/day01/input.txt") as f:
         x = [int(i) for i in f.read().strip().split("\n")]
 
-    print("Part1:", part1(x))
-    print("Part2:", part2(x))
+    print("Part1:", adjacent_increases(x))
+    print("Part2:", adjacent_increases(x, 3))
