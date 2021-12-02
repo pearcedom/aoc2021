2457d3a069d5e6541931747fc331bbf848d9f170
Dominic Pearce <Dom@Katharines-MacBook-Pro.local>
2021-12-02 08:50:15 +0000

    simplify

--- src/day02/solution.py
+++ src/day02/solution.py
@@ -1,18 +1,6 @@
 import re
 
-def part1(x):
-    horizontal = depth = 0
-    for cmd, n in x:
-        match cmd:
-            case 'forward':
-                horizontal += n
-            case 'down':
-                depth += n
-            case 'up':
-                depth -= n
-    return horizontal * depth
-
-def part2(x):
+def pilot(x):
     horizontal = depth = aim = 0
     for cmd, n in x:
         match cmd:
@@ -23,13 +11,13 @@ def part2(x):
                 aim += n
             case 'up':
                 aim -= n
-    return horizontal * depth
+    return horizontal, depth, aim
 
 if __name__ == "__main__":
     with open("src/day02/input.txt") as f:
         x = []
         for i in f.read().strip().split('\n'):
             x.append([int(i) if i.isdigit() else i for i in re.split("\s+", i)])
-        print("Part1:", part1(x))
-        print("Part2:", part2(x))
-
+        h, d, a = pilot(x)
+        print("Part1:", h * a)
+        print("Part2:", h * d)
