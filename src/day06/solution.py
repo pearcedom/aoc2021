3aa81fcf843e54017712e6c088d2934fee8ccee1
Dominic Pearce <Dom@Katharines-MacBook-Pro.local>
2021-12-06 13:10:02 +0000

    fix indentation

--- src/day06/solution.py
+++ src/day06/solution.py
@@ -13,5 +13,5 @@ def n_after(x, n):
 if __name__ == "__main__":
     with open("src/day06/input.txt") as f:
         x = [int(i) for i in f.read().strip().split(',')]
-        print("Part1:", sum(n_after(i, 80) for i in x))
-        print("Part2:", sum(n_after(i, 256) for i in x))
+    print("Part1:", sum(n_after(i, 80) for i in x))
+    print("Part2:", sum(n_after(i, 256) for i in x))
