9a5249823f1f103c4276720e2a5feb3e575c051f
Dominic Pearce <Dom@Katharines-MacBook-Pro.local>
2021-12-11 09:17:04 +0000

    tidy

--- src/day11/solution.py
+++ src/day11/solution.py
@@ -2,29 +2,21 @@ def adjacent(i, j):
     return [(i+1, j), (i-1, j), (i, j+1), (i, j-1),
             (i+1, j+1), (i+1, j-1), (i-1, j+1), (i-1, j-1)]
 
-
-def flash1(d):
-    d = d.copy()
-    for i in range(1, h+1):
-        for j in range(1, w+1):
-            d[i, j] += 1
-    return d
-
 def flash(d, h, w):
-    m = flash1(d)
+    m = d.copy()
     flashed = set()
-    to_flash = True
+    to_flash = [(i, j) for i in range(h+2) for j in range(w+2)]
 
     while to_flash:
+        for i in to_flash:
+            m[i] += 1
+
         to_flash = []
         for i in range(1, h+1):
             for j in range(1, w+1):
                 if m[i, j] > 9 and (i, j) not in flashed:
                     to_flash += adjacent(i, j)
                     flashed |= set([(i, j)])
-
-        for i in to_flash:
-            m[i] += 1
 
     n_flashes = 0
     for i in range(h+1):
@@ -37,22 +29,17 @@ def flash(d, h, w):
 
 def flash_until(d, h, w, n):
     n_flashes = 0
-    while n > 0:
+    for i in range(n):
         d, n_more = flash(d, h, w)
         n_flashes += n_more
-        n -= 1
     return n_flashes
 
 def synchronize(d, h, w):
-    n_flashes = 0
-    sq = h * w
-    n = 0
+    n, n_flashes, sq = 0, 0, h * w
     while n_flashes != sq:
         d, n_flashes = flash(d, h, w)
         n += 1
     return n
-
-
 
 if __name__ == "__main__":
     with open('src/day11/input.txt') as f:
@@ -65,9 +52,3 @@ if __name__ == "__main__":
 
     print("Part1:", flash_until(d, h, w, 100))
     print("Part2:", synchronize(d, h, w))
-
-
-
-
-
-
