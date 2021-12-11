5a1d7945f8dd53890f4cd30ab73eb1495083eec7
Dominic Pearce <Dom@Katharines-MacBook-Pro.local>
2021-12-11 17:41:23 +0000

    use numpy

--- src/day11/solution.py
+++ src/day11/solution.py
@@ -1,54 +1,42 @@
-def adjacent(i, j):
+import numpy as np
+
+def adj(i, j):
     return [(i+1, j), (i-1, j), (i, j+1), (i, j-1),
             (i+1, j+1), (i+1, j-1), (i-1, j+1), (i-1, j-1)]
 
-def flash(d, h, w):
+def flash(d):
     m = d.copy()
-    flashed = set()
-    to_flash = [(i, j) for i in range(h+2) for j in range(w+2)]
+    flashed = m > float('inf')
+    to_inc = zip(*np.where(d >= 0))
 
-    while to_flash:
-        for i in to_flash:
+    while to_inc:
+        for i in to_inc:
             m[i] += 1
+        flashing = zip(*np.where((m > 9) & ~flashed))
+        to_inc = [j for i in flashing for j in adjacent(*i)]
+        flashed |= (m > 9)
 
-        to_flash = []
-        for i in range(1, h+1):
-            for j in range(1, w+1):
-                if m[i, j] > 9 and (i, j) not in flashed:
-                    to_flash += adjacent(i, j)
-                    flashed |= set([(i, j)])
-
-    n_flashes = 0
-    for i in range(h+1):
-        for j in range(w+1):
-            if m[i, j] > 9:
-                m[i, j] = 0
-                n_flashes += 1
-
+    n_flashes = sum(sum(m > 9))
+    m[m > 9] = 0
     return m, n_flashes
 
-def flash_until(d, h, w, n):
+def flash_until(d, n):
     n_flashes = 0
     for i in range(n):
-        d, n_more = flash(d, h, w)
+        d, n_more = flash(d)
         n_flashes += n_more
     return n_flashes
 
-def synchronize(d, h, w):
-    n, n_flashes, sq = 0, 0, h * w
+def synchronize(d):
+    n, n_flashes, sq = 0, 0, (d.shape[0]-2)**2
     while n_flashes != sq:
-        d, n_flashes = flash(d, h, w)
+        d, n_flashes = flash(d)
         n += 1
     return n
 
 if __name__ == "__main__":
     with open('src/day11/input.txt') as f:
-        lines = f.read().splitlines()
-        h, w = len(lines), len(lines[0])
-        d = {(i, j): float('-inf') for i in range(h+2) for j in range(w+2)}
-        for i, j in enumerate(lines):
-            for k, l in enumerate(j):
-                d[i+1, k+1] = int(l)
+        d = np.pad(np.genfromtxt(f, delimiter=1), 1, constant_values = float('-inf'))
 
-    print("Part1:", flash_until(d, h, w, 100))
-    print("Part2:", synchronize(d, h, w))
+    print("Part1:", flash_until(d, 100))
+    print("Part2:", synchronize(d))
