f14fd0c242badf81e3441dadf76550eaf02e124f
Dominic Pearce <Dom@Katharines-MacBook-Pro.local>
2021-12-16 12:06:17 +0000

    tidy function lookup

--- src/day16/solution.py
+++ src/day16/solution.py
@@ -1,8 +1,12 @@
 from math import prod
+from operator import *
 
 d = {'0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101',
     '6': '0110', '7': '0111', '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
     'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'}
+
+fs = {0: sum, 1: prod, 2: min, 3: max,
+    5: lambda x: gt(*x), 6: lambda x: lt(*x), 7: lambda x: eq(*x)}
 
 def parse(packet):
     v, t, value = packet[0:3], int(packet[3:6], 2), packet[6:]
@@ -10,21 +14,7 @@ def parse(packet):
         return parse_literal(value)
     else:
         out, value = parse_operator(value)
-        if t == 0:
-            out = sum(out)
-        elif t == 1:
-            out = prod(out)
-        elif t == 2:
-            out = min(out)
-        elif t == 3:
-            out = max(out)
-        elif t == 5:
-            out = int(out[0] > out[1])
-        elif t == 6:
-            out = int(out[0] < out[1])
-        elif t == 7:
-            out = int(out[0] == out[1])
-        return out, value
+        return fs[t](out), value
 
 def parse_operator(value):
     lt_id, value = value[0], value[1:]
