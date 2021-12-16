2abd4471f919d2c79e57f47dc89522cefc4ca7e6
Dominic Pearce <Dom@Katharines-MacBook-Pro.local>
2021-12-16 12:13:56 +0000

    more small tidying

--- src/day16/solution.py
+++ src/day16/solution.py
@@ -5,16 +5,14 @@ d = {'0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101
     '6': '0110', '7': '0111', '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
     'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'}
 
-fs = {0: sum, 1: prod, 2: min, 3: max,
-    5: lambda x: gt(*x), 6: lambda x: lt(*x), 7: lambda x: eq(*x)}
+def reduce(out, value, t):
+    fs = {0: sum, 1: prod, 2: min, 3: max,
+        5: lambda x: gt(*x), 6: lambda x: lt(*x), 7: lambda x: eq(*x)}
+    return fs[t](out), value
 
 def parse(packet):
     v, t, value = packet[0:3], int(packet[3:6], 2), packet[6:]
-    if t == 4:
-        return parse_literal(value)
-    else:
-        out, value = parse_operator(value)
-        return fs[t](out), value
+    return parse_literal(value) if t == 4 else reduce(*parse_operator(value), t)
 
 def parse_operator(value):
     lt_id, value = value[0], value[1:]
